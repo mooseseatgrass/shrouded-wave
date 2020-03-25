f276b64b75a0682a478aee6ca5bac6834024c5a7
mooseseatgrass <skylover29@gmail.com>
2020-03-25 14:25:19 -0400

    added mysql

---
+++ for.py
@@ -0,0 +1,7 @@
+# factorial of 12, !12
+
+num = 1
+for each in range(1,13):
+    num = number * each
+
+print(num)
---
+++ loopy.py
@@ -0,0 +1,22 @@
+x = 0
+last = 0
+
+#while x ** 2 <= 999999:
+ #  if x ** 2 == int(x **2);
+#	a = x **2
+#   x += 1
+
+#print(a)
+
+#while x** <= 99999:
+#    last = x
+#    x = x + 1
+
+#print(last)
+
+start = 999999
+
+while start ** 0.5 != int(start0.5):
+    start = start - 1
+
+print(start)
