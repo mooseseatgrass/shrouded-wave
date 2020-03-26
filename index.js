9a133b7565301e087c1fdaad979697825140df51
lexiehelwig <plumlexie@gmail.com>
2020-03-26 16:49:26 -0400

    Lots of javascript that isn't working correctly.. fun fun

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
 //Validate Phone Number
 
-//get phone number input from html
+//Get info from html
 const form = document.getElementById('form');
 const phoneNumber = document.getElementById('phonenumber');
 
-//listens for form to be submitted & prevents default reload. may not be needed depending on whether we use <form> in html
+//Button Event Listener Function
 form.addEventListener('submit' , (e) => {
     e.preventDefault();
 
+    //clear form after submission
+    form.reset();
+   
+    //Validate input
     checkInputs();
 });
 
-//get phone number input
+//Check phone number input
 function checkInputs() {
     const phoneNumberValue = phoneNumber.value.trim();
 
-    //if user does not enter phone number (field is blank), display error
+    //Input Errors
     if(phoneNumberValue === '') {
         setErrorFor(phoneNumber, 'Phone number cannot be blank');
     } else if (!isPhoneNumber(phoneNumberValue)){
         setErrorFor(phoneNumber, 'Phone number is not valid');
     } else {
-        setSuccessFor(phoneNumber, 'number');
-    }
-        //add success class
-        setSuccessFor(phoneNumber, 'number');
+        setSuccessFor(phoneNumber);
     }
 
+    //display error message to user
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
     const small = formControl.querySelector('small');
 
-    //add error message inside small
+    //display error message user side
     small.innerText = message;
 
     //add error class
-    formControl.className = 'form-control error';
+    formControl.className = 'box error';
 }
 
 function setSuccessFor(input) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control success';
+    formControl.className = 'box success';
 }
 
 function isPhoneNumber(phoneNumber) {
     //this regex will validate [1]-[123]-[123]-[1234] with or without dashes, & with or without first digit
     return /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]\d{4}$/.test(phoneNumber);
-}
+}};
