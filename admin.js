669d205014d0e28f9c5d41c4bec8a5e5e104f13a
CharlesBFerguson <fergurburger326@gmail.com>
2020-03-31 06:27:18 -0400

    new code using non-existent employee DB in queries. UN/PW/DB from slack post not connecting

--- admin.js
+++ admin.js
@@ -1,24 +1,49 @@
-const http = require('http');
-const fs = require('fs');
-const port = 3000;
+const mysql = require('mysql');
+const express = require('express');
+//express required as dependency in package.json but does not seem to be installed in the node_modules folder.
+const app = express();
+const port = 3306;
+const bodyparser = require('body-parser');
+//body-parser required as dependency in package.json but does not seem to be installed in the node_modules folder.
+app.use(bodyparser.json());
 
-const server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  fs.readFile('admin.html', function(error, data) {
-    if (error) {
-      res.writeHead(404);
-      res.write('Error: File Not Found');
-    } else {
-      res.write(data);
-    }
-    res.end();
-  })
+// Username:   jgy4kfpqhzeplmwl     Password:   nal168vbk5cgeq50
+// Port:  3306    Database:   b0j93g47mct78nva
+var mysqlConnection = mysql.createConnection({
+      host : 'localhost',
+      user : 'jgy4kfpqhzeplmwl',
+  password : 'nal168vbk5cgeq50',
+  database : 'b0j93g47mct78nva'
+});
+
+mysqlConnection.connect((err) => {
+  if (!err)
+  console.log('DB connection succeded');
+  else
+  console.log ('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
 })
 
-server.listen(port, function(error) {
-  if (error) {
-    console.log('something went wrong', error);
-  } else {
-    console.log('server is listening on ' + port);
-  }
+app.listen(port, () => console.log(`Express server is running on port ${port}`));
+
+// Show in CLI:
+app.get('/employees',(res,req) => {
+  mysqlConnection.query('SELECT * FROM Employee',(err, rows, fields) => {
+    if(!err)
+    // Show entire table:
+    console.log(rows);
+    // OR show first employee ID:
+    // console.log(rows[0].EmpID);
+    else
+    console.log(err);
+  })
+}) 
+
+// Show in browser:
+app.get('/employees',(req,res) => {
+  mysqlConnection.query('SELECT * FROM Employee',(err, rows, fields) => {
+    if(!err)
+    res.send(rows);
+    else
+    console.log(err);
+  })
 })
