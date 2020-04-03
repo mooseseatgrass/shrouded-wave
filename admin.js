fec0e0a04794b31e267fc1ce433c59d42b5de832
CharlesBFerguson <fergurburger326@gmail.com>
2020-04-03 08:50:59 -0400

    added new row insertion into database

--- admin.js
+++ admin.js
@@ -7,14 +7,14 @@ app.listen(port, () => console.log(`Express server is running on port ${port}`))
 
 app.use(bodyParser.json());
 
-var mysqlConnection = mysql.createConnection({
+var dBase = mysql.createConnection({
     host : 'mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user : 'jgy4kfpqhzeplmwl',
 password : 'nal168vbk5cgeq50',
 database : 'b0j93g47mct78nva'
 })
 
-mysqlConnection.connect((err) => {
+dBase.connect((err) => {
   if (!err)
   console.log('DB connection succeded');
   else
@@ -28,6 +28,18 @@ mysqlConnection.connect((err) => {
 // I first will add code for other CRUD commands.
 // -------------------------------------------------------------------
 
+// Add row to table:
+app.get('/', (req, res) => {
+  let post = {First_Name:'Lil', Last_Name:'Wayne', Phone_Number:'706-555-3086'}; // Id_user set to Auto_Increment
+  let sql = 'INSERT INTO user SET ?';
+  let query = dBase.query(sql, post, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('Post 1 added...');
+  });
+});
+
+
 // Show in CLI:
 // 1. Must first comment out "Show in browser" code below.
 // 2. Run node admin.js in terminal.
@@ -36,15 +48,16 @@ mysqlConnection.connect((err) => {
 // 5. Will have to press Ctrl+C to end (for now).
 // Show in CLI:
 
-app.get('/',(res,req) => {
-  mysqlConnection.query('SELECT * FROM user',(err, rows, fields) => {
-    if(!err)
-    // Show entire table:
-    console.log(rows);
-    else
-    console.log(err);
-  })
-});
+// app.get('/',(res,req) => {
+//   dBase.query('SELECT * FROM user',(err, rows, fields) => {
+//     if(!err)
+//     // Show entire table:
+//     console.log(rows);
+//     else
+//     console.log(err);
+//   })
+// });
+
 
 // Show in browser:
 // 1. Must first comment out "show in CLI" code above.
@@ -53,7 +66,7 @@ app.get('/',(res,req) => {
 // Show in browser:
 
 // app.get('/',(req,res) => {
-//   mysqlConnection.query('SELECT * FROM user',(err, rows, fields) => {
+//   dBase.query('SELECT * FROM user',(err, rows, fields) => {
 //     if(!err)
 //     res.send(rows);
 //     else
@@ -63,4 +76,4 @@ app.get('/',(res,req) => {
 
 
 // DO NOT ENABLE THIS OR WILL NOT WORK
-// mysqlConnection.end();
+// dBase.end();
